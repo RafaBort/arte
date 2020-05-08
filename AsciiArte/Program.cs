fa6170396bb3951cf110484e7968e2261316577a
RafaBort <rafaelafonseca.bort2019ri@gmail.com>
2020-05-08 20:14:23 -0300

    exercício resolvido

---
+++ AsciiArte/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AsciiArte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("||  (_)  ||");
+            Console.WriteLine(" \\(___)//");
+            Console.WriteLine("//(_____)\\");
+            Console.WriteLine("||       ||");
+        }
+    }
+}
